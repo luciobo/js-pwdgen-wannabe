3f5741e51fd5cd1e7d39d9e419e8000acf0ef6ac
luciobo <luciobo@icloud.com>
2022-10-26 17:26:50 +0200

    creata struttura

--- js/user.js
+++ js/user.js
@@ -1,3 +1,15 @@
-const userName = prompt("Inserisci il tuo nome completo");
-const userSurame = prompt("Inserisci il tuo nome completo");
-const userColor = prompt("Inserisci il tuo nome completo");
+const userName = prompt("Inserisci il tuo nome");
+const userSurame = prompt("Inserisci il tuo Cognome");
+const userColor = prompt("Inserisci il tuo colore preferito");
+
+const h2NomeUntente = document.getElementById ("nome_utente");
+
+h2NomeUntente.innerHTML = (userName + "" + userSurame)
+
+const paragrafo = document.getElementById ("paragrafo");
+
+paragrafo.innerHTML = (userName + userSurame + userColor + 21)
+
+
+
+
